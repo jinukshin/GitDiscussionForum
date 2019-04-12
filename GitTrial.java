4578fbefc38403893e206107cd6e91198ba407f9
jinukshin <jinuk@seas.upenn.edu>
2019-04-11 22:22:22 -0700

    Created a file for git collaboration discussion

---
+++ GitTrial.java
@@ -0,0 +1,17 @@
+
+public class GitTrial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String start = "This file is created ";
+		String middle = "to test Git with collaborater!";
+		String end = "Can you find the typo I have on all my typings and fix it ? :-) ";
+		
+		System.out.println(start + middle);
+		System.out.println(end);
+		System.out.println("Bye~~~");
+		
+	}
+
+}
